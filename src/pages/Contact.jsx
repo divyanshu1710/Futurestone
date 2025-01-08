b1ae3fb79176095835f0a506d9174b12e83e509c
divyanshu1710 <divyasha1701@gmail.com>
2025-01-08 22:12:22 +0530

    gradient changes

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,17 +1,18 @@
-import React from 'react'
+import React from 'react';
 import { Container } from "react-bootstrap";
 import Particle from '../components/Particle';
 import ContactForm from '../components/Contact/Contact';
 import Social from '../components/Contact/Social';
+import '../style.css'; // Import your CSS file
 
 const Contact = () => {
   return (
-    <Container style={{padding: '60px'}}>
+    <Container className="contact-container">
       <Particle />
       <ContactForm />
       <Social />
     </Container>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
