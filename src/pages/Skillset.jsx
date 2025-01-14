4ae0085f13a61ac6676cfe89527218d004a824fe
divyanshu1710 <divyasha1701@gmail.com>
2025-01-14 10:23:47 +0530

    stylings

--- src/pages/Skillset.jsx
+++ src/pages/Skillset.jsx
@@ -4,33 +4,54 @@ import '../style.css';
 const Gallery = () => {
   const [carouselIndex, setCarouselIndex] = useState(0);
   const [expandedGroups, setExpandedGroups] = useState([]); // Tracks expanded groups
-  const groupHeaders = ["Monuments", "HeadStone", "Weasis"];
+  const groupHeaders = ["Headstone", "Coverstone", "Vases"];
 
   const keyProducts = [
     {
-      img: "./assets/image1.jpg",
-      description: "Small Stone Monuments are versatile, functional, and equally aesthetic pieces, often used as nameplates or smaller memorials. These compact stone structures bring the same timeless beauty and elegance as their larger counterparts but in a more personal and accessible form. Whether placed at the entrance of a home, business, or memorial site, these monuments make a lasting impression.",
+      video: "./assets/coverstone.mp4"
     },
     {
-      img: "./assets/image2.jpg",
-      description: "Stone Accessories bring an element of natural elegance and charm to any space, transforming ordinary decor into extraordinary expressions of style. From decorative stone carvings, candle holders, and vases to small statues, these accessories offer a touch of sophistication for home, garden, or office environments.",
+      video: "./assets/headstone.mp4"
     },
     {
-      img: "./assets/image3.jpg",
-      description: "Big Monuments are grand, meticulously crafted stone pieces that serve as timeless symbols of remembrance and honor. Typically used for cemetery monuments, they stand as powerful tributes to loved ones, offering a dignified presence in memorial spaces.",
+      video: "./assets/vases.mp4"
     },
   ];
 
   const products = [
-    { id: 1, img: "./assets/download (1).jpg", colors: ["red", "blue", "green"], areas: ["US", "EU", "Asia"], group: 1 },
-    { id: 2, img: "./assets/download (2).jpg", colors: ["yellow", "purple"], areas: ["EU", "Asia"], group: 1 },
-    { id: 3, img: "./assets/download (3).jpg", colors: ["yellow", "purple"], areas: ["EU", "Asia"], group: 1 },
-    { id: 4, img: "./assets/DSC01192.JPG", colors: ["yellow", "purple"], areas: ["EU", "Asia"], group: 1 },
-    { id: 5, img: "./assets/DSC01221.JPG", colors: ["yellow", "purple"], areas: ["EU", "Asia"], group: 1 },
-    { id: 6, img: "./assets/DSC01323.JPG", colors: ["yellow", "purple"], areas: ["EU", "Asia"], group: 2 },
-    { id: 7, img: "./assets/DSC01554.JPG", colors: ["yellow", "purple"], areas: ["EU", "Asia"], group: 3 },
-    { id: 8, img: "./assets/DSC01618.JPG", colors: ["yellow", "purple"], areas: ["EU", "Asia"], group: 3 },
-    { id: 9, img: "./assets/DSC02131.JPG", colors: ["yellow", "purple"], areas: ["EU", "Asia"], group: 3 },
+    { id: 1, img: "./assets/32.png", colors: ["red", "blue", "green"], areas: ["US", "EU", "Asia"], group: 3 },
+    { id: 2, img: "./assets/33.png", colors: ["yellow", "purple"], areas: ["EU", "Asia"], group: 3 },
+    { id: 3, img: "./assets/34.png", colors: ["yellow", "purple"], areas: ["EU", "Asia"], group: 3 },
+    { id: 4, img: "./assets/35.png", colors: ["yellow", "purple"], areas: ["EU", "Asia"], group: 3 },
+    { id: 5, img: "./assets/36.png", colors: ["yellow", "purple"], areas: ["EU", "Asia"], group: 3 },
+    { id: 6, img: "./assets/37.png", colors: ["yellow", "purple"], areas: ["EU", "Asia"], group: 3 },
+    { id: 7, img: "./assets/38.png", colors: ["yellow", "purple"], areas: ["EU", "Asia"], group: 3 },
+    { id: 8, img: "./assets/39.png", colors: ["yellow", "purple"], areas: ["EU", "Asia"], group: 3 },
+
+    { id: 9, img: "./assets/3.png", colors: ["yellow", "purple"], areas: ["EU", "Asia"], group: 2 },
+    { id: 10, img: "./assets/6.png", colors: ["yellow", "purple"], areas: ["EU", "Asia"], group: 2 },
+    { id: 11, img: "./assets/7.png", colors: ["yellow", "purple"], areas: ["EU", "Asia"], group: 2 },
+    { id: 12, img: "./assets/8.png", colors: ["yellow", "purple"], areas: ["EU", "Asia"], group: 2 },
+    { id: 13, img: "./assets/9.png", colors: ["yellow", "purple"], areas: ["EU", "Asia"], group: 2 },
+    { id: 14, img: "./assets/10.png", colors: ["yellow", "purple"], areas: ["EU", "Asia"], group: 2 },
+    { id: 15, img: "./assets/12.png", colors: ["yellow", "purple"], areas: ["EU", "Asia"], group: 2 },
+    { id: 16, img: "./assets/16.png", colors: ["yellow", "purple"], areas: ["EU", "Asia"], group: 2 },
+    { id: 17, img: "./assets/17.png", colors: ["yellow", "purple"], areas: ["EU", "Asia"], group: 2 },
+    { id: 18, img: "./assets/18.png", colors: ["yellow", "purple"], areas: ["EU", "Asia"], group: 2 },
+    { id: 19, img: "./assets/19.png", colors: ["yellow", "purple"], areas: ["EU", "Asia"], group: 2 },
+    { id: 20, img: "./assets/20.png", colors: ["yellow", "purple"], areas: ["EU", "Asia"], group: 2 },
+
+    { id: 21, img: "./assets/2.png", colors: ["yellow", "purple"], areas: ["EU", "Asia"], group: 1 },
+    { id: 23, img: "./assets/22.png", colors: ["yellow", "purple"], areas: ["EU", "Asia"], group: 1 },
+    { id: 21, img: "./assets/23.png", colors: ["yellow", "purple"], areas: ["EU", "Asia"], group: 1 },
+    { id: 22, img: "./assets/24.png", colors: ["yellow", "purple"], areas: ["EU", "Asia"], group: 1 },
+    { id: 23, img: "./assets/25.png", colors: ["yellow", "purple"], areas: ["EU", "Asia"], group: 1 },
+    { id: 21, img: "./assets/26.png", colors: ["yellow", "purple"], areas: ["EU", "Asia"], group: 1 },
+    { id: 22, img: "./assets/27.png", colors: ["yellow", "purple"], areas: ["EU", "Asia"], group: 1 },
+    { id: 23, img: "./assets/28.png", colors: ["yellow", "purple"], areas: ["EU", "Asia"], group: 1 },
+    { id: 21, img: "./assets/29.png", colors: ["yellow", "purple"], areas: ["EU", "Asia"], group: 1 },
+    { id: 22, img: "./assets/30.png", colors: ["yellow", "purple"], areas: ["EU", "Asia"], group: 1 },
+    { id: 23, img: "./assets/31.png", colors: ["yellow", "purple"], areas: ["EU", "Asia"], group: 1 },
   ];
 
   const groupProducts = (products) => {
@@ -63,7 +84,7 @@ const Gallery = () => {
   useEffect(() => {
     const interval = setInterval(() => {
       setCarouselIndex((prevIndex) => (prevIndex + 1) % keyProducts.length);
-    }, 10000);
+    }, 5000);
     return () => clearInterval(interval);
   }, [keyProducts.length]);
 
@@ -71,25 +92,24 @@ const Gallery = () => {
     <div className="gallery-container">
       {/* Main Carousel */}
       <div className="carousel-container">
-        <button className="carousel-btn prev" onClick={handlePrevMain}>&lt;</button>
-        <div className="carousel-image-container">
-          <img
-            src={keyProducts[carouselIndex].img}
-            alt={`Key Product ${carouselIndex + 1}`}
-            className="carousel-image"
-          />
-          <div className="carousel-overlay">
-            <h2>
-              {carouselIndex === 0
-                ? "Small Stone Monuments"
-                : carouselIndex === 1
-                ? "Stone Accessories"
-                : "Big Stone Monuments"}
-            </h2>
-            <p>{keyProducts[carouselIndex].description}</p>
-          </div>
+        <button className="carousel-btn prev" onClick={handlePrevMain}>
+          &lt;
+        </button>
+        <div className="carousel-video-container">
+          <video
+            src={keyProducts[carouselIndex].video}
+            controls
+            autoPlay
+            loop
+            className="carousel-video"
+          ></video>
+          <p className="carousel-description">
+            {keyProducts[carouselIndex].description}
+          </p>
         </div>
-        <button className="carousel-btn next" onClick={handleNextMain}>&gt;</button>
+        <button className="carousel-btn next" onClick={handleNextMain}>
+          &gt;
+        </button>
       </div>
 
       {/* Gallery Groups */}
