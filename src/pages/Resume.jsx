3bc17ebd4f9f4072a9c8cfb334bce4758522491c
345AYUSH420 <singhalayush40@gmail.com>
2025-01-21 16:21:45 +0530

    ADDING BROCHURE

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -4,14 +4,17 @@ import Button from "react-bootstrap/Button";
 import { AiOutlineDownload } from "react-icons/ai";
 
 import Particle from '../components/Particle'
-import pdf from "../assets/sajib.pdf"
+import pdf from "../assets/Fortune Stonex Brochure 2023.pdf"
 
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import "react-pdf/dist/esm/Page/TextLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
-const resumeLink = `https://raw.githubusercontent.com/19sajib/portfolio/main/src/assets/sajib.pdf`
+const resumeLink = `https://raw.githubusercontent.com/345AYUSH420/fortunestoneX_brochure/main/Fortune%20Stonex%20Brochure%202023.pdf`
+
+
+
 
 
 const Resume = () => {
@@ -34,25 +37,7 @@ const Resume = () => {
             style={{ maxWidth: "250px" }}
           >
             <AiOutlineDownload />
-            &nbsp;Download Resume
-          </Button>
-        </Row>
-
-        <Row className="resume">
-          <Document file={resumeLink} className="d-flex justify-content-center">
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
-          </Document>
-        </Row>
-
-        <Row style={{ justifyContent: "center", position: "relative" }}>
-          <Button
-            variant="primary"
-            href={pdf}
-            target="_blank"
-            style={{ maxWidth: "250px" }}
-          >
-            <AiOutlineDownload />
-            &nbsp;Download Resume
+            &nbsp;Download Brochure
           </Button>
         </Row>
       </Container>
