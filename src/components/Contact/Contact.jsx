7464e5cca58a77472f9cdc5002bcca34e3a7fbb8
345AYUSH420 <singhalayush40@gmail.com>
2025-01-22 13:52:53 +0530

    changing the email feature

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -176,7 +176,7 @@ const Contact = () => {
     emailjs
       .sendForm(
         "service_yvvxebq",
-        "template_f1bbmqr",
+        "template_uo9p5hf",
         form.current,
         "Z2aD1bBk8nIIad12x"
       )
@@ -207,7 +207,7 @@ const Contact = () => {
                 <textarea name="message" className="user" placeholder="Message" onChange={handleChange} />
                 <span className='not-done' >{notDone && "Please, fill all the input field"}</span>
                 <Button type="submit" className="button" disabled={done}>Send</Button>
-                <span className='done'>{done && "Thanks for contacting me and be sure i have recieved your mail. If you are testing this functionality then i am confirming this thing working perfectly fine. If you have any serious query then i will reply. Also if you need me, you can conatct me on Linkedin."}</span>
+                <span className='done'>{done && "Thanks For Your Request !! We Will Contact You As Soon As Possible"}</span>
                 </form>
             </Col>
             </Row>
